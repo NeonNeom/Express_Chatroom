d4bb11fff826fff4f1290fb77eb29c72c4ce5600
neonneom <deandreholmes55@gmail.com>
2018-10-06 11:44:35 -0400

    added theme select functionality

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
     //Make connection
     var socket = io.connect('http://localhost:4000');
+    var themeColor = 'l';
+    var classHolder;
+
+    $("#lightTheme").click(function(){
+        if(themeColor != 'l'){
+            $("body").removeClass("darker");
+            $("#chat-window, #sendMessage, .messageContainer").removeClass("dark");
+            $("#theme").css("border", "1px solid rgb(228, 217, 217, .15)");
+            classHolder = 'l';
+            themeColor = 'l';
+        }
+    });
+
+    $("#darkTheme").click(function(){
+        if(themeColor != 'd'){
+            $("body").addClass("darker");
+            $("#chat-window, #sendMessage, .messageContainer").addClass("dark");
+            $(".messageContainer").removeClass("l");
+            $("#theme").css("border", "1px solid rgb(228, 217, 217, .1)");
+            classHolder = 'dark';
+            themeColor = 'd';
+        }
+    });
+
+    $("#message").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#sendMessage").click();
+        }
+    });
 
     $("#sendMessage").click(function(){
         if($("#message").val().trim() !=""){
@@ -24,7 +53,7 @@ $(document).ready(function() {
 
     socket.on('chat', function(data){
         $("#feedback").html("");
-        $("#output").append("<div class=\"messageContainer\"> <p class=\"chatName\">" + data.username + " </p> <p class=\"chatTimestamp\">" + data.date + "</p> <p class=\"chatMessage\">" + data.message + "</p> </div>");
+        $("#output").append("<div class=\"messageContainer " + classHolder + "\"> <p class=\"chatName\">" + data.username + " </p> <p class=\"chatTimestamp\">" + data.date + "</p> <p class=\"chatMessage\">" + data.message + "</p> </div>");
     });
 
     socket.on('typing', function(data){
